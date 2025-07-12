10123c7bbb83032d8f46b62c34c4a0047289d085
Nada Shaban <nadashaban971@gmail.com>
2025-07-12 19:22:33 +0300

    add optimal solution for Two Sum

--- problems/1.TwoSum.js
+++ problems/1.TwoSum.js
@@ -1,15 +1,30 @@
 // leetCode 1 - Two Sum
 
 // brute force
-//complexity : o(n^2)
+// Time Complexity: O(n^2)
 function twoSum(arr , target){
     for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length && i!==j; j++){
+        for(let j = i + 1; j < arr.length; j++){
             if(arr[i] + arr[j] === target){
                 return [i,j];
             }
         }
     }
-};
+}
 
-console.log(twoSum([2,7,1,2] , 9));
+// optimal soln using hashMap
+// Time Complexity: O(n)
+function twoSum2(arr , target){
+    let map = new Map();
+    for(let i = 0; i < arr.length; i++){
+        const ans = target - arr[i];
+        if(map.has(ans)){
+            return [map.get(ans), i];
+        }
+        map.set(arr[i], i);
+    }
+}
+
+// Test cases
+console.log(twoSum([2, 7, 1, 2], 9));   // [0, 1]
+console.log(twoSum2([2, 7, 1, 2], 9));  // [0, 1]
