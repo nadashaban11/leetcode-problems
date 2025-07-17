9748ebd102d2d12c5106e88fa647551424cb965d
Nada Shaban <nadashaban971@gmail.com>
2025-07-17 23:21:03 +0300

    414. Third Maximum Number

---
+++ problems/414.ThirdMaximumNumber.js
@@ -0,0 +1,23 @@
+// leetCode  414 - Third Maximum Number
+
+var thirdMax = function(nums) {
+    nums.sort((a,b)=>b-a);
+    const set = new Set(nums);
+    let c = 1;
+    if(nums.length <= 2 || set.size <= 2)
+        return nums[0];
+    for(const i of set){
+        if(c === 3){
+            return i;
+        }
+        c++;
+    }
+
+};
+
+// test cases
+
+console.log(thirdMax([3, 2, 1]));       // 1
+console.log(thirdMax([1, 2]));          // 2
+console.log(thirdMax([2, 2, 3, 1]));    // 1
+console.log(thirdMax([5, 2, 2]));       // 5
