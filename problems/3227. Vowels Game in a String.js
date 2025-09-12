55b1626723f72ab0e1d15cfdea27b88210a142c7
Nada Shaban <nadashaban971@gmail.com>
2025-09-12 15:20:52 +0300

    Add problem 3227. Vowels Game in a String

---
+++ problems/3227. Vowels Game in a String.js
@@ -0,0 +1,36 @@
+// leetCode 3227. Vowels Game in a String
+
+var doesAliceWin = function(s) {
+    s = s.split('');
+    if(cntVowels(s) === 0)
+        return false;
+    // if(cntVowels(s) % 2) 
+    else
+        return true;
+
+};
+
+var isVowel = function(c){
+    return (c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u');
+};
+
+var cntVowels = function(s){
+    let counter = 0;
+    s.forEach((ch) => {
+        if(isVowel(ch)) counter++;
+    });
+    return counter;
+}
+
+// more efficient soln
+
+var doesAliceWin = function(s) {
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u')
+            return true;
+    }
+    return false;
+};
+
+
+
