cc76d2ccc0ccfb8fd6de42d13c81f32dccace264
Nada Shaban <nadashaban971@gmail.com>
2025-11-23 23:20:38 +0200

    more efficient soln

--- problems/326. Power of Three.c
+++ problems/326. Power of Three.c
@@ -1,14 +1,22 @@
 // leetCode 326. Power of Three
-
+//both with (almost) same time comp
+//but the iterative one less stack space waste:) 
 bool isPowerOfThree(int n) {
-    if(n <= 0){
-        return false;
-    }
-    if(n == 1){
-        return true;
-    }
-    if(n % 3 != 0){
-        return false;
-    }
-    return isPowerOfThree(n/3);
+    if (n <= 0) return false;
+    while (n % 3 == 0) n /= 3;
+    return n == 1;
 }
+
+
+// bool isPowerOfThree(int n) {
+//     if(n <= 0){
+//         return false;
+//     }
+//     if(n == 1){
+//         return true;
+//     }
+//     if(n % 3 != 0){
+//         return false;
+//     }
+//     return isPowerOfThree(n/3);
+// }
