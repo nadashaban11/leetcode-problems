96503b9c8348e87efbe76ad0703c4749f2e73b85
Nada Shaban <nadashaban971@gmail.com>
2025-11-23 23:14:31 +0200

    high time but i want to practice rec

---
+++ problems/326. Power of Three.c
@@ -0,0 +1,14 @@
+// leetCode 326. Power of Three
+
+bool isPowerOfThree(int n) {
+    if(n <= 0){
+        return false;
+    }
+    if(n == 1){
+        return true;
+    }
+    if(n % 3 != 0){
+        return false;
+    }
+    return isPowerOfThree(n/3);
+}
