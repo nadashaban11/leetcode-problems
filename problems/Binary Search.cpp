001815fd39f864744a2abf6dd9211608f5c8b3cd
Nada Shaban <nadashaban971@gmail.com>
2025-12-01 23:33:37 +0200

    Add problem Binary Search

---
+++ problems/Binary Search.cpp
@@ -0,0 +1,27 @@
+
+// NeetCode Binary Search
+// Time : O(log n)
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0, r = nums.size() - 1;
+        int mid;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (target == nums[mid])
+                return mid;
+            if (target > nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return -1;
+    }
+};
